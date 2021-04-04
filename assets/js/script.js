c83c4d4e0697d28b508f471b3ce4ac3aa9816e7b
Adam Arthur <adamarthur44@gmail.com>
2021-04-04 20:59:22 +0100

    added Onload Function from Local Storage

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const bodyElement = document.body;
+const dateTime = document.getElementById("currentDay");
+const currentHour = moment().hour();
+// Get data from Local Storage
+const nineEvent = localStorage.getItem("Nine");
+const tenEvent = localStorage.getItem("Ten");
+const elevenEvent = localStorage.getItem("Eleven");
+const twelveEvent = localStorage.getItem("Twelve");
+const oneEvent = localStorage.getItem("One");
+const twoEvent = localStorage.getItem("Two");
+const threeEvent = localStorage.getItem("Three");
+const fourEvent = localStorage.getItem("Four");
+const fiveEvent = localStorage.getItem("Five");
+
+// OnLoad Function
+$(document).ready(function  () {
+    function update() {
+        $("#currentDay").text(moment().format("dddd Do MMMM, HH:mm:ss"));
+    }
+    setInterval(update, 1000);
+    $("#nine").text(nineEvent);
+    $("#ten").text(tenEvent);
+    $("#eleven").text(elevenEvent);
+    $("#twelve").text(twelveEvent);
+    $("#one").text(oneEvent);
+    $("#two").text(twoEvent);
+    $("#three").text(threeEvent);
+    $("#four").text(fourEvent);
+    $("#five").text(fiveEvent);
+})
